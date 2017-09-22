a82f57f80d4c9dc92f29a6e8ec8badc5c014033b
= <b02166@ut.ee>
2017-09-22 13:48:02 +0300

    2nd script updated

--- slurm-scripts/submission1/2-shuffle-split-data.sh
+++ slurm-scripts/submission1/2-shuffle-split-data.sh
@@ -6,17 +6,21 @@
 
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
-#SBATCH -t 00:10:00
+#SBATCH -t 01:00:00
 
 # tab-join and shuffle et and en corpora
 paste raw-all.{et,en} | shuf > mixed-data.both
+echo "Shuffle complete"
 
 # split corpus
 sed -n 1,50000p mixed-data.both | cut -f 1 > test.et
 sed -n 1,50000p mixed-data.both | cut -f 2 > test.en
+echo "Test sets complete"
 sed -n 50001,75000p mixed-data.both | cut -f 1 > dev.et
 sed -n 50001,75000p mixed-data.both | cut -f 2 > dev.en
+echo "Dev sets complete"
 sed -n 75001,19051439p mixed-data.both | cut -f 1 > train.et
 sed -n 75001,19051439p mixed-data.both | cut -f 2 > train.en
+echo "Trains sets complete"
 
 echo "Job finished"
