513f9d66f5abc556430913d89c0e62d26010308f
Markus Kängsepp <markus9696@gmail.com>
2017-09-27 11:50:23 +0300

    7. script - fix

--- slurm-scripts/submission1/7-preprocess.sh
+++ slurm-scripts/submission1/7-preprocess.sh
@@ -2,19 +2,12 @@
 
 # The location of this file is in data/ folder where raw-all folder is.
 
-#SBATCH -J quidditch1_tiny16
+#SBATCH -J quidditch1_tiny17
 
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH -t 24:00:00
 #SBATCH --mem=16G
-
-#SBATCH --mem=50G
-
-#Leave this here if you need a GPU for your job
-#SBATCH --partition=gpu
-
-#SBATCH --gres=gpu:tesla:1
 
 #The module with all the NMT / deep learning packages
 module load python-2.7.13
