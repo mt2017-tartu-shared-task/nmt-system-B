ecdfcb7276d8a0d66f9cab5befd091b87eff0167
Markus Kängsepp <markus93@ut.ee>
2017-10-18 15:41:40 +0300

    Some changes to de_bpe

--- hyps/de_bpe.py
+++ hyps/de_bpe.py
@@ -8,5 +8,10 @@ with open(fin, "r") as f:
         with open(gout, "w") as g:
                 for line in f:
                         line = line.strip()
-                        line = re.sub(r"@@ ", "", line)
+                        line = re.sub(r"@@ ", "", line)                        
+                        line = re.sub(r"&amp;", "&", line)
+                        line = re.sub(r"& quot ;", "\"", line)
+                        line = re.sub(r"& amp ;", "&", line)
+                        line = re.sub(r"& lt ;", "<", line)
+                        line = re.sub(r"& gt ;", ">", line)
                         g.write(line + "\n")
