fe2afe4c95879e5d7c361072fa1d2522b8a1e630
Markus Kängsepp <markus93@ut.ee>
2017-10-18 20:03:59 +0300

    Added necessary data files for analyses

--- hyps/de_truecase_simple.py
+++ hyps/de_truecase_simple.py
@@ -3,8 +3,8 @@ import sys
 fin = sys.argv[1]
 gout = sys.argv[2]
 
-with open(fin, "r", encoding="utf-8") as f:
-        with open(gout, "w", encoding="utf-8") as g:
+with open(fin, "r") as f:
+        with open(gout, "w") as g:
                 for line in f:
                     if len(line) != 0:
                         g.write(line[0].upper() + line[1:])
