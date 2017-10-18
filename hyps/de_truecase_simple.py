3e1d8d1c835aae2d16b7f54eb04c0ecf1503bac7
Markus Kängsepp <markus9696@gmail.com>
2017-10-18 19:18:57 +0300

    Added simple de-truecasing script.

---
+++ hyps/de_truecase_simple.py
@@ -0,0 +1,10 @@
+import sys
+
+fin = sys.argv[1]
+gout = sys.argv[2]
+
+with open(fin, "r", encoding="utf-8") as f:
+        with open(gout, "w", encoding="utf-8") as g:
+                for line in f:
+                    if len(line) != 0:
+                        g.write(line[0].upper() + line[1:])
